d42c819c38e37ed03f09aa2159bbd0ac4005c355
alek3y <alek3y@protonmail.com>
2023-08-13 20:05:43 +0200

    Add basic matrix functions

--- vnn.h
+++ vnn.h
@@ -28,14 +28,59 @@
 #define VNN_DTYPE_DIV(a, b) ((a) / (b))
 #endif
 
+#ifdef VNN_EXTERN
+#define VNNDEF extern
+#else
+#define VNNDEF static
+#endif
+
 typedef struct {
 	VNN_DTYPE *data;
-	size_t cols, rows;
+	size_t rows, cols;
 } Matrix;
+
+VNNDEF Matrix matrix_new(VNN_DTYPE *data, size_t rows, size_t cols);
+VNNDEF void matrix_free(Matrix *dest);
+VNNDEF Matrix matrix_copy(Matrix src);
+VNNDEF Matrix matrix_transpose(Matrix src);
+
+#define MATRIX_AT(src, i, j) (src).data[(i)*(src).cols + (j)]
 
 typedef struct {
 	Matrix *weights;
 	size_t layers;
 } Network;
+
+VNNDEF Matrix matrix_new(VNN_DTYPE *data, size_t rows, size_t cols) {
+	Matrix dest = {data, rows, cols};
+	return matrix_copy(dest);
+}
+
+VNNDEF void matrix_free(Matrix *dest) {
+	assert(dest->data != NULL);
+	VNN_FREE(dest->data);
+	dest->data = NULL;
+}
+
+VNNDEF Matrix matrix_copy(Matrix src) {
+	size_t size = src.cols*src.rows * sizeof(VNN_DTYPE);
+	Matrix dest = {VNN_MALLOC(size), src.rows, src.cols};
+	memcpy(dest.data, src.data, size);
+	return dest;
+}
+
+VNNDEF Matrix matrix_transpose(Matrix src) {
+	Matrix dest = matrix_copy(src);
+	dest.rows = src.cols;
+	dest.cols = src.rows;
+
+	for (size_t i = 0; i < src.rows; i++) {
+		for (size_t j = 0; j < src.cols; j++) {
+			MATRIX_AT(dest, j, i) = MATRIX_AT(src, i, j);
+		}
+	}
+
+	return dest;
+}
 
 #endif
