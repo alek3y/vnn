3d2a9826f1691b97caca00d04b2c211d0408fb1e
alek3y <alek3y@protonmail.com>
2023-08-17 02:23:25 +0200

    Add XOR learning example

---
+++ examples/xor.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
+#include "vnn.h"
+
+float weights() {
+	return (float) rand() / RAND_MAX;
+}
+
+float activation(float excitation) {
+	return 1.0/(1.0 + exp(-excitation));
+}
+
+float derivative(float excitation) {
+	return exp(-excitation)/pow(1.0 + exp(-excitation), 2);
+}
+
+int main(void) {
+	srand(time(NULL));
+
+	const size_t epochs = 10000;	// Small epochs show slow learning for certain starting weights
+
+	const Matrix inputs[] = {
+		matrix_from((float[]) {0, 0}, 1, 2),
+		matrix_from((float[]) {0, 1}, 1, 2),
+		matrix_from((float[]) {1, 0}, 1, 2),
+		matrix_from((float[]) {1, 1}, 1, 2)
+	};
+
+	const Matrix targets[] = {
+		matrix_from((float[]) {0}, 1, 1),
+		matrix_from((float[]) {1}, 1, 1),
+		matrix_from((float[]) {1}, 1, 1),
+		matrix_from((float[]) {0}, 1, 1)
+	};
+
+	Network nn = network_new(
+		(size_t[]) {2, 2, 1}, 3, 1,
+		(VNN_DTYPE (*[])(VNN_DTYPE)) {activation, activation},
+		(VNN_DTYPE (*[])(VNN_DTYPE)) {derivative, derivative},
+		weights
+	);
+
+	size_t samples = sizeof(inputs)/sizeof(inputs[0]);
+	for (size_t e = 1; e <= epochs; e++) {
+		size_t sample = rand() % samples;
+		network_feed(nn, inputs[sample]);
+
+		float error = network_error(nn, targets[sample]);
+		printf("Epoch %lu/%lu, Error: %g\r", e, epochs, error);
+
+		network_adjust(nn, targets[sample]);
+	}
+	printf("\n");
+
+	for (size_t i = 0; i < samples; i++) {
+		Matrix input = inputs[i];
+		printf("Input {%g, %g}", MATRIX_AT(input, 0, 0), MATRIX_AT(input, 0, 1));
+		Matrix output = network_feed(nn, input);
+		printf(", Output {%g, %g}\n", MATRIX_AT(output, 0, 0), MATRIX_AT(output, 0, 1));
+	}
+
+	network_free(&nn);
+}
